bc8f76332a6038d54b941cd93b7c7a4161190d45
Isabella <collantemendezisabella@gmail.com>
2023-08-17 10:38:48 -0600

    Feat(Home): First Home

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,10 +1,112 @@
 <link rel="stylesheet" href="<?= base_url('css/home.css') ?>">
+<link rel="stylesheet" href="<?= base_url('css/aos.css') ?>">
 <main class="contenedor">
     <div class="contenedor-productos mt-5">
         <!-- PRODUCTOS DINAMICOS -->
     </div>
+
+    <!-- CATEGORIAS -->
+    <section class="d-flex justify-content-center align-items-center flex-column">
+        <h1>Categorias</h1>
+        <div class="container d-flex justify-content-center flex-column w-100">
+            <div class="card mb-3" style="max-width: 540px;" id="card-categoria" data-aos="fade-right">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src=<?php echo base_url("img/logo-asoca-s.png") ?> class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title">Moda</h3>
+                            <hr>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis iure possimus animi temporibus aspernatur, facere magnam blanditiis praesentium libero aliquam voluptate expedita officia labore culpa obcaecati quisquam? Ab, libero labore. .</p>
+                            <button class="btn btn-primary">Ver mas
+                                <i data-lucide-name="chevron-right"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3" style="max-width: 540px;" id="card-categoria" data-aos="fade-left">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src=<?php echo base_url("img/logo-asoca-s.png") ?> class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title">Belleza</h3>
+                            <hr>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis iure possimus animi temporibus aspernatur, facere magnam blanditiis praesentium libero aliquam voluptate expedita officia labore culpa obcaecati quisquam? Ab, libero labore. .</p>
+                            <button class="btn btn-primary">Ver mas
+                                <i data-lucide-name="chevron-right"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3" style="max-width: 540px;" id="card-categoria" data-aos="fade-right">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src=<?php echo base_url("img/logo-asoca-s.png") ?> class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title">Accesorios</h3>
+                            <hr>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis iure possimus animi temporibus aspernatur, facere magnam blanditiis praesentium libero aliquam voluptate expedita officia labore culpa obcaecati quisquam? Ab, libero labore. .</p>
+                            <button class="btn btn-primary">Ver mas
+                                <i data-lucide-name="chevron-right"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3" style="max-width: 540px;" id="card-categoria" data-aos="fade-left">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src=<?php echo base_url("img/logo-asoca-s.png") ?> class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title">Gourmet</h3>
+                            <hr>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis iure possimus animi temporibus aspernatur, facere magnam blanditiis praesentium libero aliquam voluptate expedita officia labore culpa obcaecati quisquam? Ab, libero labore. .</p>
+                            <button class="btn btn-primary">Ver mas
+                                <i data-lucide-name="chevron-right"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card mb-3" style="max-width: 540px;" id="card-categoria" data-aos="fade-right">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src=<?php echo base_url("img/logo-asoca-s.png") ?> class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h3 class="card-title">Hogar </h3>
+                            <hr>
+                            <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis iure possimus animi temporibus aspernatur, facere magnam blanditiis praesentium libero aliquam voluptate expedita officia labore culpa obcaecati quisquam? Ab, libero labore. .</p>
+                            <button class="btn btn-primary">Ver mas
+                                <i data-lucide-name="chevron-right"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
 </main>
 
+<script src="<?= base_url("js/aos.js") ?>"></script>
+<script>
+    AOS.init();
+</script>
 <script>
     $.ajax({
         url: `${url}obtenerProductos`,
@@ -13,7 +115,7 @@
         data: {
             estado: 'A'
         },
-        success: function (res) {
+        success: function(res) {
             var cadena = '';
             switch (res.length) {
                 case 0:
