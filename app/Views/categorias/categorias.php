6f8280acdccc4d95143bbeb234a1c0414ab96a47
MDMS29 <mdmazo30@misena.educ.co>
2023-09-11 18:40:28 -0500

    Fet(Categorías): Nuevo componente de categorías

--- app/Views/categorias/categorias.php
+++ app/Views/categorias/categorias.php
@@ -1,15 +1,15 @@
+<link rel="stylesheet" href="<?= base_url('css/categorias/categorias.css')?>">
 <div class="contenedor">
     <div id="content" class="p-4 p-md-5">
         <section class="d-flex justify-content-between">
             <h2 class="text-capitalize fw-semibold">Productos - <?= $categoria ?> - </h2>
             <button class="btn btn-primary" onclick="history.back()"><i class="bi bi-arrow-left"></i> Volver</button>
         </section>
-        <div id="contenedor-productos-categoria">
-            <!-- PRODUCTOS DINAMICOS -->
-
-        </div>
-        <div class="contenedor-paginacion">
-            <button class="paginacion-categorias btn btn-outline-dark" id="1">1</button>
+        <div class="swiper" id="swiper-categorias">
+            <div class="swiper-wrapper" id="wrapper-cate">
+                <!-- PRODUCTOS DINÁMICOS -->
+            </div>
+            <div class="swiper-pagination"></div>
         </div>
     </div>
     <footer style="background-color: #C1AE9F;position:relative;bottom:-200; width:100%;">
@@ -58,44 +58,58 @@
         },
         success: function(res) {
             var cadena = '';
-
+            let i = 0
             if (res[0]?.categoria == '<?= $categoria ?>') {
-                let i = 4
-                do {
-                    cadena += '<div class="grid-responsive mt-4" >'
-                    for (i; i < res.length; i++) {
-                        var foto = `${url}imagenesProducto/${res[i].nombre_img}`;
+                let a = 10
+                cadena += '<div class="swiper-slide">'
+                for (i; i <= a; i++) {
+                    if (res[i]?.id_producto == undefined) {
+                        cadena += ''
+                    } else {
+                        var foto = `${url}imagenesProducto/${res[i]?.nombre_img}`;
                         cadena += `
-                            <div onclick="window.location.href='<?= base_url('detalles-producto/') ?>${res[i].id_producto}'" class="card mb-3" style="cursor:pointer;border-radius:15px;">
+                            <div onclick="window.location.href='<?= base_url('detalles-producto/') ?>${res[i]?.id_producto}'" class="card mb-3" style="cursor:pointer;border-radius:15px;max-height:330px;">
                                 <div class="row g-0">
                                     <div class="col-md-4 d-flex justify-content-center w-100 py-3" >
-                                    <img src="${foto}"alt="${res[i].nombre_img}" width="130" height="150">
+                                    <img src="${foto}"alt="${res[i]?.nombre_img}" width="130" height="150">
                                     </div>
                                     <div class="card-body text-center">
-                                        <h5 class="card-title text-capitalize fw-bold" style="display: -webkit-box;-webkit-box-orient: vertical;-webkit-line-clamp: 1; overflow: hidden;"> ${res[i].nombre}</h5>
-                                        <p class="card-text fw-semibold text-danger">${formatearCantidad(res[i].precio)} COP <span class="text-secondary">c/u</span></p>
-                                        <small class="text-secondary text-capitalize">- ${res[i].categoria} -</small>
+                                        <h5 class="card-title text-capitalize fw-bold" style="display: -webkit-box;-webkit-box-orient: vertical;-webkit-line-clamp: 1; overflow: hidden;"> ${res[i]?.nombre}</h5>
+                                        <p class="card-text fw-semibold text-danger">${formatearCantidad(res[i]?.precio)} COP <span class="text-secondary">c/u</span></p>
+                                        <small class="text-secondary text-capitalize">- ${res[i]?.categoria} -</small>
                                     </div>
                                 </div>
                             </div>`
                     }
-                    cadena += '</div>'
-                    if (i == 4) {
+                    let newA = a - 1
+                    if (i == newA) {
+                        cadena += `</div>`
+                        a = res.length
                         cadenas.push(cadena)
+                        cadena += '<div class="swiper-slide">'
                     }
-
-                } while (res.length < i);
-                console.log(cadenas)
+                    for (let e = 0; e < cadenas.length; e++) {
+                        $('#wrapper-cate').html(cadenas[e])
+                    }
+                    crearSwiper("swiper-categorias", {
+                        slidesPerView: 1,
+                        grid: {
+                            rows: 1
+                        },
+                        spaceBetween: 30,
+                        pagination: {
+                            el: ".swiper-pagination",
+                            clickable: true
+                        }
+                    })
+                }
             } else {
                 cadena += `<div class="container text-center mt-5" style="grid-column: 1/7;">
-                                <p>NO HAY PRODUCTOS EN ESTA CATEGORÍA DE <span class="text-uppercase"><?= $categoria ?></span></p>
-                                <a href="<?= base_url() ?>" class="btn btn-dark">Descubre más aquí</a>
-                            </div>`
-
+                <p>NO HAY PRODUCTOS EN ESTA CATEGORÍA DE <span class="text-uppercase"><?= $categoria ?></span></p>
+                <a href="<?= base_url() ?>" class="btn btn-dark">Descubre más aquí</a>
+                </div>`
             }
 
-            $('#contenedor-productos-categoria').html(cadena)
-            // $('.grid-responsive').html(cadena)
         }
     })
 </script>
